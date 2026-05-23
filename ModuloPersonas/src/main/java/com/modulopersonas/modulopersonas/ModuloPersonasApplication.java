516f9eba936a265e5f7d0b57a39747561f96bf8c
leiner calvo <leinercalvo0@gmail.com>
2026-05-23 11:59:15 -0500

    ProyectoCreado

---
+++ ModuloPersonas/src/main/java/com/modulopersonas/modulopersonas/ModuloPersonasApplication.java
@@ -0,0 +1,13 @@
+package com.modulopersonas.modulopersonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloPersonasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuloPersonasApplication.class, args);
+    }
+
+}
---
+++ ModuloPersonas/src/test/java/com/modulopersonas/modulopersonas/ModuloPersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modulopersonas.modulopersonas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloPersonasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
